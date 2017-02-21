e259a523691df4177d7e8f2e12f63a8cff62b99a
Ben Booth <benwbooth@gmail.com>
2017-02-21 14:28:19 -0800

    modularize

---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+#![recursion_limit = "1024"]
+use std::str;
+use std::vec::Vec;
+
+extern crate regex;
+
+extern crate theban_interval_tree;
+extern crate memrange;
+
+extern crate rust_htslib;
+use rust_htslib::bam::record::Cigar;
+
+#[macro_use]
+extern crate error_chain;
+
+pub mod errors {
+    error_chain!{
+        foreign_links {
+            Io(::std::io::Error) #[cfg(unix)];
+            Utf8(::std::str::Utf8Error);
+            Regex(::regex::Error);
+            ReaderPath(::rust_htslib::bam::ReaderPathError);
+        }
+        errors {
+            NoneError
+        }
+    }
+    pub trait ToResult<T> {
+        fn r(self) -> Result<T>;
+    }
+    impl<T> ToResult<T> for Option<T> {
+        fn r(self) -> Result<T> {
+            match self {
+                Some(v) => Ok(v),
+                None => Err(ErrorKind::NoneError.into()),
+            }
+        }
+    }
+}
+
+use errors::*;
+
+pub fn cigar2exons(exons: &mut Vec<(u64, u64)>, cigar: &[Cigar], pos: u64) -> Result<()> {
+    let mut pos = pos;
+    for op in cigar {
+        match op {
+            &Cigar::Match(length) => {
+                pos += length as u64;
+                exons.push((pos - length as u64, pos));
+                Ok(())
+            }
+            &Cigar::RefSkip(length) |
+            &Cigar::Del(length) => {
+                pos += length as u64;
+                Ok(())
+            }
+            &Cigar::Ins(_) |
+            &Cigar::SoftClip(_) |
+            &Cigar::HardClip(_) |
+            &Cigar::Pad(_) => Ok(()),
+            c => Err(format!("Bad CIGAR string: {:?}", c)),
+        }?;
+    }
+    Ok(())
+}
